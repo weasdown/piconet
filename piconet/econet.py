a4ec3965f77e8f743710d355353ff40d76e4a4ab
weasdown <34220924+weasdown@users.noreply.github.com>
2025-09-15 20:13:34 +0100

    feat: initial commit

---
+++ piconet/econet.py
@@ -0,0 +1,16 @@
+# See https://en.wikipedia.org/wiki/Econet#Network_and_transport_layers
+
+class Header:
+    pass
+
+
+class Packet:
+    pass
+
+
+class Frame:
+    pass
+
+
+class Transmission:
+    pass
