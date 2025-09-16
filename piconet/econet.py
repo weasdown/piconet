15c5e8462374eda1a45e5fabf88297a63f52825d
weasdown <34220924+weasdown@users.noreply.github.com>
2025-09-16 12:06:02 +0100

    feat: add EconetInterface class

--- piconet/econet.py
+++ piconet/econet.py
@@ -1,4 +1,6 @@
-# See https://en.wikipedia.org/wiki/Econet#Network_and_transport_layers
+# See https://en.wikipedia.org/wiki/Econet, especially the "Physical and data-link layers" and "Network and transport layers" sections.
+
+from dataclasses import dataclass
 
 class Header:
     pass
@@ -14,3 +16,12 @@ class Frame:
 
 class Transmission:
     pass
+
+@dataclass
+class EconetInterface:
+    # Pin definitions
+    data_pos: int   # +ve
+    ground: int
+    clock_pos: int  # +ve
+    data_neg: int   # -ve
+    clock_neg: int  # -ve
